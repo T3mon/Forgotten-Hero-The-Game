50a349abec781112911213d2983034069aeb52d0
Artem <44963781+T3mon@users.noreply.github.com>
2023-02-09 00:29:23 +0200

    Add attacking

--- Forgotten Hero The Game/Assets/Enemy.cs
+++ Forgotten Hero The Game/Assets/Enemy.cs
@@ -1,39 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-namespace Main
+
+public class Enemy : MonoBehaviour
 {
-    public class Enemy : MonoBehaviour
+    public float health;
+
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
     {
-        public float health;
+        animator = GetComponent<Animator>();
+    }
 
-        private Animator animator;
-        // Start is called before the first frame update
-        void Start()
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
         {
-            animator = GetComponent<Animator>();
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-            if (health <= 0)
-            {
-                Die();
-            }
-        }
-
-        public void TakeDamage(float damage)
-        {
-            Debug.Log("slime hp: " + health);
-            health -= damage;
-        }
-
-        private void Die()
-        {
-            animator.SetTrigger("defeated");
-            Debug.Log("slime dead");
-            Destroy(gameObject);
+            Die();
         }
     }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        Debug.Log("slime hp: " + health);
+    }
+
+    private void Die()
+    {
+        animator.SetTrigger("defeated");
+    }
+    
+    private void RemoveEnemy()
+    {
+        Destroy(gameObject);
+    }
+    
 }
--- Forgotten Hero The Game/Assets/SwordAttack.cs
+++ Forgotten Hero The Game/Assets/SwordAttack.cs
@@ -2,70 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Main
+public partial class SwordAttack : MonoBehaviour
 {
-    public class SwordAttack : MonoBehaviour
+    public float damage = 3f;
+    public Collider2D swordCollider;
+    private Vector3 rightAttackOffset;
+    // private PlayerController playerController;
+
+    // Start is called before the first frame update
+    void Start()
     {
-        public float damage = 3f;
-        public Collider2D swordCollider;
+        // playerController = GetComponent<PlayerController>();
+        swordCollider.enabled = false;
+        rightAttackOffset = transform.position; // attack offset is right by default
+    }
 
-        private Direction mouseDirection;
-        private enum Direction { up, down, left, right } // *TODO Move it to namespace level 
-        private Vector3 rightAttackOffset;
-        // private PlayerController playerController;
+    public void Stop()
+    {
+        swordCollider.enabled = false;
+    }
 
-        // Start is called before the first frame update
-        void Start()
+    public void Attack(Direction direction)
+    {
+        swordCollider.enabled = true;
+        switch (direction) // Vector values are hardcore to be pixel perfect
         {
-            swordCollider = GetComponent<Collider2D>();
-            // playerController = GetComponent<PlayerController>();
-            swordCollider.enabled = false;
-            rightAttackOffset = transform.position; // attack offset is right by default
+            case Direction.left:
+                transform.localPosition = new Vector3(rightAttackOffset.x * -1, rightAttackOffset.y);
+                break;
+            case Direction.right:
+                transform.localPosition = rightAttackOffset;
+                break;
+            case Direction.up:
+                transform.localPosition = new Vector3(0, -0.08f);
+                break;
+            case Direction.down:
+                transform.localPosition = new Vector3(0.02f, -0.14f);
+                break;
+            default:
+                break;
         }
+    }
 
-        public void Stop()
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Enemy")
         {
-            swordCollider.enabled = false;
-        }
+            Enemy enemy = other.GetComponent<Enemy>();
+            if (enemy != null)
+                enemy.TakeDamage(damage);
 
-        public void AttackRight() // ! delete this
-        {
-            Debug.Log("test");
-        }
-
-        public void Attack()
-        {
-            swordCollider.enabled = true;
-
-            switch (mouseDirection) // Vector values are hardcore to be pixel perfect
-            {
-                case Direction.left:
-                    transform.position = new Vector3(rightAttackOffset.x * -1, rightAttackOffset.y);
-                    break;
-                case Direction.right:
-                    transform.position = rightAttackOffset;
-                    break;
-                case Direction.up:
-                    transform.position = new Vector3(0, -0.08f);
-                    break;
-                case Direction.down:
-                    transform.position = new Vector3(0.02f, -0.14f);
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        private void OnTriggerEnter2D(Collider2D other)
-        {
-            if (other.tag == "Enemy")
-            {
-                Enemy enemy = other.GetComponent<Enemy>();
-                if (enemy != null)
-                    enemy.TakeDamage(damage);
-
-                //do damage
-            }
+            //do damage
         }
     }
 }
